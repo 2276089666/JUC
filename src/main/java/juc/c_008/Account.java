15ae89ace714a655664bdbfbf52df8e59007dff3
2276089666 <56677272+2276089666@users.noreply.github.com>
2021-05-11 22:38:45 +0800

    同步方法和非同步方法同时调用导致的问题

---
+++ src/main/java/juc/c_008/Account.java
@@ -0,0 +1,56 @@
+/**
+ * 面试题：模拟银行账户
+ * 对业务写方法加锁
+ * 对业务读方法不加锁
+ * 这样行不行？
+ *
+ * 容易产生脏读问题（dirtyRead）
+ */
+
+package juc.c_008;
+
+import java.util.concurrent.TimeUnit;
+
+public class Account {
+	String name;
+	double balance;
+	
+	public synchronized void set(String name, double balance) {
+		this.name = name;
+
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		
+		this.balance = balance;
+	}
+	
+	public /*synchronized*/ double getBalance(String name) {
+		return this.balance;
+	}
+	
+	
+	public static void main(String[] args) {
+		Account a = new Account();
+		new Thread(()->a.set("zhangsan", 100.0)).start();
+		
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println(a.getBalance("zhangsan"));  // 由于同步方法和非同步方法可以同时运行,读的时候,钱没加进去,脏读
+		
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println(a.getBalance("zhangsan")); // 这个时间点,一定会读到跟新了的钱数
+	}
+}
