b92bfcb1e122e4b32c702c76e47bcd53c1cd7b35
2276089666 <56677272+2276089666@users.noreply.github.com>
2021-05-12 21:24:47 +0800

    修改之前的错误

--- src/main/java/JustTest.java
+++ src/main/java/JustTest.java
@@ -3,10 +3,10 @@ public class JustTest {
         // view 里面 show Bytecode 创建对象的几大过程
         /**
          *     LINENUMBER 3 L0
-         *     NEW java/lang/Object               分配内存
+         *     NEW java/lang/Object               分配内存(基本数值类型有默认值)
          *     DUP
-         *     INVOKESPECIAL java/lang/Object.<init> ()V       建立栈中和堆中的引用连接
-         *     ASTORE 1                           调构造函数赋初值
+         *     INVOKESPECIAL java/lang/Object.<init> ()V     调构造函数赋初值
+         *     ASTORE 1                           建立栈中和堆中的引用连接
          */
         Object o = new Object();
     }
