2378f46ad2710200253c5e2802d00e0f92b0e04a
2276089666 <56677272+2276089666@users.noreply.github.com>
2021-05-13 22:02:01 +0800

    2个线程之间交换数据的Exchanger

---
+++ src/main/java/juc/c_020/T12_TestExchanger.java
@@ -0,0 +1,39 @@
+package juc.c_020;
+
+/**
+ * 线程之间交换数据
+ */
+
+import java.util.concurrent.Exchanger;
+
+public class T12_TestExchanger {
+
+    static Exchanger<String> exchanger = new Exchanger<>();
+
+    public static void main(String[] args) {
+        new Thread(()->{
+            String s = "T1";
+            try {
+                s = exchanger.exchange(s); // 阻塞,等待数据交换完后,就放行(只限2个线程之间交换数据)
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(Thread.currentThread().getName() + " " + s);
+
+        }, "t1").start();
+
+
+        new Thread(()->{
+            String s = "T2";
+            try {
+                s = exchanger.exchange(s);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println(Thread.currentThread().getName() + " " + s);
+
+        }, "t2").start();
+
+
+    }
+}
