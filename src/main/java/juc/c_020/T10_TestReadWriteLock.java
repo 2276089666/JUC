3e5788cec041fa1ab14ac28a2e701ffde3c1a788
2276089666 <56677272+2276089666@users.noreply.github.com>
2021-05-13 22:01:19 +0800

    读写锁

---
+++ src/main/java/juc/c_020/T10_TestReadWriteLock.java
@@ -0,0 +1,66 @@
+package juc.c_020;
+/**
+ * 读写锁
+ * 写+写 后面的阻塞
+ * 读+写 后面的阻塞
+ * 写+读 后面的阻塞
+ * 读+读 不阻塞
+ */
+
+import java.util.Random;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+public class T10_TestReadWriteLock {
+    static Lock lock = new ReentrantLock();
+    private static int value;
+
+    static ReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+    static Lock readLock = readWriteLock.readLock();
+    static Lock writeLock = readWriteLock.writeLock();
+
+    public static void read(Lock lock) {
+        try {
+            lock.lock();
+            Thread.sleep(1000);
+            System.out.println("read over!");
+            //模拟读取操作
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public static void write(Lock lock, int v) {
+        try {
+            lock.lock();
+            Thread.sleep(1000);
+            value = v;
+            System.out.println("write over!");
+            //模拟写操作
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+
+
+    public static void main(String[] args) {
+        // 使用可重入锁
+//        Runnable readR = ()-> read(lock);
+//        Runnable writeR = ()->write(lock, new Random().nextInt());
+        // 使用读写锁
+        Runnable readR = ()-> read(readLock);
+        Runnable writeR = ()->write(writeLock, new Random().nextInt());
+
+        for(int i=0; i<18; i++) new Thread(readR).start();
+        for(int i=0; i<2; i++) new Thread(writeR).start();
+
+
+    }
+}
